b92d47459c2eb332beaf4cd1da6e1419f6669f24
tecshuttle <tecshuttle@gmail.com>
2021-05-14 09:02:58 +0800

    项目表单验证。

--- src/View/ProjectForm.vue
+++ src/View/ProjectForm.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
     <div>
-      <a-button type="primary" @click="cancel"> 返回 </a-button>
+      <a-form ref="formRef" :model="formState" :rules="rules" :label-col="{ span: 6 }" :wrapper-col="{ span: 17 }">
+        <a-form-item ref="name" name="name" label="项目名称">
+          <a-input v-model:value="formState.name" />
+        </a-form-item>
 
-      <a-form :form="form" :label-col="{ span: 6 }" :wrapper-col="{ span: 17 }">
-        <input v-model="name" />
-        <a-form-item :wrapper-col="{ span: 12, offset: 5 }">
-          <a-button type="primary" @click="save"> 保存 </a-button>
+        <a-form-item :wrapper-col="{ span: 14, offset: 4 }">
+          <a-button type="primary" @click="onSubmit">新建</a-button>
+          <a-button style="margin-left: 10px" @click="goBack">取消</a-button>
         </a-form-item>
       </a-form>
     </div>
@@ -14,53 +16,68 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
 import axios from "axios";
+import { ValidateErrorEntity } from 'ant-design-vue/es/form/interface';
+import { useRouter } from "vue-router"
+import { defineComponent, ref, UnwrapRef, reactive, toRaw } from "vue";
+
+interface FormState {
+  name: string;
+}
 
 export default defineComponent({
-  components: {},
-  data() {
-    return {
-      name: "",
-      formVal: {
-        name: "",
-      },
-      formLayout: "horizontal",
-      form: "", //this.$form.createForm(this, { name: "coordinated" }),
-      data: "",
+  setup() {
+    const formRef = ref();
+    const formState: UnwrapRef<FormState> = reactive({
+      name: ''
+    });
+
+    const rules = {
+      name: [
+        { required: true, message: '请输入项目名称', trigger: 'blur' },
+        { min: 2, max: 50, message: '字符长度：2-50', trigger: 'blur' },
+      ]
     };
-  },
-  mounted() {
-    this.getData();
-  },
-  methods: {
-    getData() {
-      axios
-        .get("/api/projects")
-        .then((res) => {
-          this.data = res.data.data;
+
+    const onSubmit = () => {
+      formRef.value
+        .validate()
+        .then(() => {
+          axios
+            .post("/api/projects", toRaw(formState))
+            .then((res) => {
+              goBack();
+            })
+            .catch((err) => {
+              console.log(err);
+            });
         })
-        .catch((err) => {
-          console.log(err);
+        .catch((error: ValidateErrorEntity<FormState>) => {
+          console.log('error', error);
         });
-    },
-    cancel() {
-      this.return();
-    },
-    return() {
-      this.$router.push({ name: "Project" });
-    },
-    save() {
-      axios
-        .post("/api/projects", { name: this.name })
-        .then((res) => {
-          this.return();
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    },
-  },
+    };
+
+    const router = useRouter();
+
+    const goBack = () => {
+      router.push({ name: "Project" });
+    };
+
+    return {
+      formRef,
+      rules,
+      labelCol: {
+        span: 4,
+      },
+      wrapperCol: {
+        span: 14,
+      },
+      formState,
+      onSubmit,
+      router,
+      goBack
+    };
+  }
 });
 </script>
 
